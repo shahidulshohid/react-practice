33ae8cf5caf09a4cfdd9e68070122c954f973665
shahidul shohid <shahidulislamshohid7gmail.com>
2024-10-14 23:26:17 +0600

    first commit

---
+++ modu-37-recap-js/task.js
@@ -0,0 +1,57 @@
+// 1. how to declare a variable using let and const
+const fatherName = 'Arnold'
+let season = 'rainy'
+season = 'winter'
+
+// 2. 6 conditions <, >, <=, >=, === !==
+// multiple conditions
+// if(fatherName === 'arnold'){
+//     console.log('I have got the specific name')
+// }
+// else if(fatherName === 'Arnold'){
+//     console.log('I have got specific name')
+// }
+// else{
+//     console.log("I didn't have get specific name")
+// }
+
+// 3. array declare 
+// index
+// length, push
+// const numbers = [89, 35, 98, 12]
+// console.log(numbers.length);
+// numbers[1] = 100
+// numbers[3] = 55
+// console.log(numbers)
+// numbers.push(15)
+// console.log('============', numbers.push(10))
+// console.log(numbers)
+// console.log('===============', numbers.pop())
+// console.log(numbers)
+// numbers.shift()
+// numbers.unshift(300)
+// console.log(numbers)
+// for(let i = 0; i < numbers.length; i++){
+    // console.log(i)
+    // console.log(numbers[i])
+// }
+// console.log(numbers.indexOf(98))
+// console.log(numbers.indexOf(500))
+// console.log(numbers.includes(98))
+// console.log(numbers.includes(500))
+
+const array = [33, 43, 55, 12, 44]
+
+// const arrSlice = array.slice(1, 3)
+// const arrSlice1 = array.slice(0, 4)
+// console.log(arrSlice)
+// console.log(arrSlice1)
+// console.log(array.splice(2, 4))
+// array.splice(0,3,4,5,65)
+// const result = array.join()
+// const result1 = array.join(' ')
+// const result2 = array.join('-')
+// console.log(result)
+// console.log(result1)
+// console.log(result2)
+// console.log(array)
