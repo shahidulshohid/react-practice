6600b6896da37ce97176337697da2532e4443745
shahidul shohid <shahidulislamshohid7gmail.com>
2024-10-15 00:30:55 +0600

    some practice is done

---
+++ modu-37-recap-js/fundamental.js
@@ -0,0 +1,20 @@
+ 
+ // 5 function
+function multiple(num1, num2){
+    const result = num1 * num2
+    return result;
+}
+const output = multiple(20,2)
+// console.log(output)
+
+// object
+// 3 ways to access property by name
+const student = {
+    name:'shakib khan',
+    age: 32,
+    movies: ['king khan', 'dhakar mastan']
+}
+const myVariable = 'age'
+console.log(student.age) //direct by property
+console.log(student['age']) // access vai property name string
+console.log(student[myVariable])
