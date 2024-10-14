4ad3ed1878ec15aaca10283179758aa12f0e8207
shahidul shohid <shahidulislamshohid7gmail.com>
2024-10-15 00:45:33 +0600

    some practice is done

---
+++ modu-37-recap-js/es6.js
@@ -0,0 +1,25 @@
+
+const numbers = [89, 35, 98, 12]
+const student = {
+    name: 'shakib khan',
+    age: 32,
+    movies: ['king khan', 'dhakar mastan']
+}
+// 1 template string 
+const about = `my name is ${student.name} age of ${student.age} has number ${numbers[2]} also liked movie`;
+// console.log(about)
+
+// 2 arrow function 
+const getFiftyFive = () => 55
+// console.log(getFiftyFive())
+const addSixtyFive = num => num + 65
+// console.log(addSixtyFive(2)) 
+const isEven = x => x % 2 === 0
+// console.log(isEven(10))
+const addThree = (x, y, z) => x + y + z
+// console.log(addThree(2, 3, 4))
+const doMath = (num1, num2) => {
+    const sum = num1 + num2
+    return sum
+}
+console.log(doMath(2, 6))
