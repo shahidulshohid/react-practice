d043724ed3a3ccaeea2e5c21ff53e6b24521e670
shahidul shohid <shahidulislamshohid7gmail.com>
2024-10-16 12:41:25 +0600

    react practice is done

--- modu-38-react-start/my-first-react/src/Todo.jsx
+++ modu-38-react-start/my-first-react/src/Todo.jsx
@@ -18,9 +18,42 @@
 // }
 
 //condition rendering option 2
+// export default function Todo({task, isDone}){
+//    if(isDone){
+//     return <li>Finish: {task}</li>
+//    }
+//    return <li>Work on {task}</li>
+// }
+
+// conditional rendering option 3 
+// export default function Todo({task, isDone}){
+//    return (
+//       <li>{isDone ? 'Finished' : 'Work on'} : {task}</li>
+//    )
+// }
+
+// conditional rendering option 4 
+// export default function Todo({task, isDone}){
+//    return (
+//       <li>{task} {isDone && ': Done'}</li>
+//    )
+// }
+
+// conditional rendering option 5
+// export default function Todo({task, isDone}){
+//    return (
+//       <li>{task} {isDone || ': Done'}</li>
+//    )
+// }
+
+// conditional rendering option 6 
 export default function Todo({task, isDone}){
+   let listItem;
    if(isDone){
-    return <li>Finish: {task}</li>
+      listItem = <li>Finish: {task}</li>
    }
-   return <li>Work on {task}</li>
+   else{
+      listItem = <li>Work on: {task}</li>
+   }
+   return listItem;
 }
