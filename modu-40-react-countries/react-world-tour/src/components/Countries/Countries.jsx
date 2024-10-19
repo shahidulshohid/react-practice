db0009730e4a46095e7242a4f0e87150de5752a3
shahidul shohid <shahidulislamshohid7gmail.com>
2024-10-20 02:46:23 +0600

    module 40 preactice is done

---
+++ modu-40-react-countries/react-world-tour/src/components/Countries/Countries.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+import Country from '../Country/Country';
+import './Countries.css'
+
+const Countries = () => {
+
+    const [countries, setCountries] = useState([])
+    const [visitedCountries, setVisitedCountries] = useState([])
+
+  
+    useEffect(() => {
+      fetch("https://restcountries.com/v3.1/all")
+      .then(res => res.json())
+      .then(data => setCountries(data))
+    }, [])
+    
+    const handleVisitedCountry = country => {
+        const newVisitedCountries = [...visitedCountries, country]
+        setVisitedCountries(newVisitedCountries)
+    }
+    console.log(visitedCountries);
+    
+
+    return (
+        <div>
+            <h3>Countries: {countries.length}</h3>
+            <div>
+            <h5>Visited countries: {visitedCountries.length} </h5>
+            <ul>
+                {
+                    visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+                }
+            </ul>
+            </div>
+            <div className='country-container'>
+            {
+                countries.map(country => <Country key={country.cca3} country={country} handleVisitedCountry={handleVisitedCountry}></Country>)
+            }
+            </div>
+        </div>
+    );
+};
+
+export default Countries;
