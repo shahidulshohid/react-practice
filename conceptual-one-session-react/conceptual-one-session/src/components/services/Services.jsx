d6ad364ad032cf87cd075c9acd6461d1c124f02d
shahidul shohid <shahidulislamshohid7gmail.com>
2024-10-19 10:16:41 +0600

    conceptual class prctice is done

--- conceptual-one-session-react/conceptual-one-session/src/components/services/Services.jsx
+++ conceptual-one-session-react/conceptual-one-session/src/components/services/Services.jsx
@@ -1,9 +1,10 @@
 export default function Services({transfer}){
     // console.log(transfer)
+    const obj = {name: 'shahidul islam', age:33}
     return (
         <div>
         <h1>I am from Services</h1>
-        <button onClick={()=>transfer('data dilam ami services theke')}>click from services</button>
+        <button onClick={()=>transfer(obj)}>click from services</button>
         </div>
     )
 }
